614d5429999352ee0df22cefd2e91d79d85038c2
BrunoBenedetto <bruno7.benedetto@gmail.com>
2026-01-27 17:21:30 -0400

    realizando teste

---
+++ EstudosIniciais/src/CodigosBasicos/testeNoMain.java
@@ -0,0 +1,11 @@
+package CodigosBasicos;
+
+public class testeNoMain {
+    public static void main(String[] args) {
+        System.out.println("Realizando teste");
+
+        /*teste de commit*/
+
+        //17-20-teste//
+    }
+}
