ebe7add7d4fa6f7f41542680f596504666818ddd
BrunoBenedetto <bruno7.benedetto@gmail.com>
2026-01-28 09:58:23 -0400

    git add maquina profissional

--- EstudosIniciais/src/CodigosBasicos/commitAdd.java
+++ EstudosIniciais/src/CodigosBasicos/commitAdd.java
@@ -4,5 +4,6 @@ public class commitAdd {
     public static void main(String[] args) {
 
         System.out.println("commit add");
+        System.out.println("commit add máquina profissional");
     }
 }
